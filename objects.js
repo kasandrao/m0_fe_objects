f83728c5b51e6e49f1da6fbfaec6d79709d42bf7
Kasandra Orta <kasandraorta26@gmail.com>
2023-06-08 18:09:52 -0600

    Initial commit

--- objects.js
+++ objects.js
@@ -13,19 +13,23 @@ var foods = {
 
 // Write code that prints all of the 'keys' of the foods variable 
 // you created above:
-// YOUR CODE HERE
+
+console.log(Object.keys(foods));
 
 // Write code that prints all of the 'values' of the foods variable 
 // you created above:
-// YOUR CODE HERE
+
+console.log(Object.values(foods))
 
 // Write code that prints the value of the second food of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(foods.grapes);
 
 // Write code that adds a food to the foods object. 
 // Then, print the updated object:
-// YOUR CODE HERE
+foods.bananas = 3;
+console.log(Object.keys(foods));
+
 
 
 // -------------------
@@ -38,17 +42,28 @@ var foods = {
 
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+var email1 = {
+  sender: "Macy's",
+  subject: "Sale",
+  words: 1000,
+  spam: false,
+  read: true
+
+};
 
 // Write code that logs your email object to the terminal.
+console.log(email1)
 
 
 // Write code that logs all of the 'keys' of the email object 
 // you created above:
-// YOUR CODE HERE
+console.log(Object.keys(email1));
 
 // Write code that logs all of the 'values' of the email object 
 // you created above:
-// YOUR CODE HERE
+
+
+console.log(Object.values(email1))
 
 
 // -------------------
@@ -99,3 +114,31 @@ console.log(posts[0]);
 // YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+
+
+emails = [
+  {
+    sender: "Macy's",
+    subject: "Sale",
+    words: 1000,
+    spam: false,
+    read: true
+  }, 
+  {
+    sender: "Saks",
+    subject: "New inventory",
+    words: 50,
+    spam: false,
+    read: true
+  },
+  { 
+    sender: "Barnes and Noble",
+    subject: "Tour",
+    words: 600,
+    spam: false,
+    read: false
+  }
+   
+];
+
+console.log(emails);
